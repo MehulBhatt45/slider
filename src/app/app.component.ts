0f53e5b1b8258e8d2ea93b4da5fdc724c08c51a6
mehulbhatt45 <mehul.2287884@gmail.com>
2019-03-21 00:16:45 +0530

    changes in slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import * as enLocale from 'date-fns/locale/en';
 })
 export class AppComponent implements OnInit {
 	title = 'app';
-	color = ['green', 'yellow', 'orange'];
+	image = ['../assets/cumbria-web.jpg', '../assets/nothern-web.jpg', '../assets/welcome-couple.jpg'];
 	couter = 0;
 	startDate;
 	endDate;
@@ -21,9 +21,9 @@ export class AppComponent implements OnInit {
 		firstCalendarDay: 0, // 0 - Sunday, 1 - Monday
 		locale: enLocale,
 		minDate: new Date(Date.now()), // Minimal selectable date
-		placeholder: 'Click to select a date', // HTML input placeholder attribute (default: '')
+		placeholder: 'Check in', // HTML input placeholder attribute (default: '')
 		addClass: 'form-control', // Optional, value to pass on to [ngClass] on the input field
-		addStyle: {}, // Optional, value to pass to [ngStyle] on the input field
+		addStyle: {'padding-left': '25px'}, // Optional, value to pass to [ngStyle] on the input field
 		fieldId: 'my-date-picker', // ID to assign to the input field. Defaults to datepicker-<counter>
 		useEmptyBarTitle: false, // Defaults to true. If set to false then barTitleIfEmpty will be disregarded and a date will always be shown 
 	};
@@ -36,23 +36,35 @@ export class AppComponent implements OnInit {
 		firstCalendarDay: 0, // 0 - Sunday, 1 - Monday
 		locale: enLocale,
 		minDate: this.startDate?this.addDays(this.startDate, 1):new Date(Date.now()), // Minimal selectable date
-		placeholder: 'Click to select a date', // HTML input placeholder attribute (default: '')
+		placeholder: 'Check out', // HTML input placeholder attribute (default: '')
 		addClass: 'form-control', // Optional, value to pass on to [ngClass] on the input field
-		addStyle: {}, // Optional, value to pass to [ngStyle] on the input field
+		addStyle: {'padding-left': '25px'}, // Optional, value to pass to [ngStyle] on the input field
 		fieldId: 'my-date-picker', // ID to assign to the input field. Defaults to datepicker-<counter>
 		useEmptyBarTitle: false, // Defaults to true. If set to false then barTitleIfEmpty will be disregarded and a date will always be shown
 	};
 	ngOnInit(){
 		var main = document.getElementById('main'); 
-		main.style.backgroundColor = this.color[this.couter];
+		// main.style.backgroundColor = this.image[this.couter];
+		main.style.backgroundImage = "url('"+this.image[this.couter]+"')";
+		main.style.backgroundRepeat = "no-repeat";
+		main.style.backgroundSize = "cover";
+		main.style.backgroundAttachment = "fixed";
 		main.style.transition = "all 3s";
 		setInterval(()=>{
-			this.couter++;
-			if(this.couter == 4)
+			if(this.couter == 4){
 				this.couter = 0;
-			document.getElementById('main').style.backgroundColor = this.color[this.couter];
-		},3000);
+				main.style.backgroundImage = "url('"+this.image[this.couter]+"')";
+			}else{
+				main.style.backgroundImage = "url('"+this.image[this.couter]+"')";
+			}
+			this.couter++;
+		},5000);
 
+	}
+
+	onChange(event){
+		console.log(this.startDate);
+		this.endoptions.minDate = this.addDays(this.startDate, 1);
 	}
 
 	addDays(date, days) {
